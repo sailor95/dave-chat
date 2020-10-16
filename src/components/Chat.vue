f51aa3695409e32d4c0984f997a7102583876356
David Huang <bugman195@gmail.com>
2020-10-16 13:45:49 +0800

    Add blank Chat comp

---
+++ src/components/Chat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="chat container">
+    <h2>Chat</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Welcome from '@/components/Welcome';
+import Chat from '@/components/Chat';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Welcome',
       component: Welcome,
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
     },
   ],
 });
